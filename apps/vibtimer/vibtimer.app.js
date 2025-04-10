5d2c6d1e3eb2a2c57ba3bed4b50da32bb0321331
v2lentina <115621639+v2lentina@users.noreply.github.com>
2025-04-10 15:12:04 +0200

    ble always active

--- apps/vibtimer/vibtimer.app.js
+++ apps/vibtimer/vibtimer.app.js
@@ -2,6 +2,9 @@ g.clear();
 g.setFont("4x6", 2);
 g.drawString("VibTimer ready", 40, 60);
 
+Bangle.setLCDTimeout(0);
+Bangle.setPowerSave(false);
+
 const SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
 const CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
 
@@ -24,3 +27,15 @@ NRF.setServices({
     }
   }
 }, { advertise: [SERVICE_UUID] });
+
+NRF.on('disconnect', () => {
+  g.clear();
+  g.drawString("BLE getrennt", 10, 30);
+  print("BLE getrennt");
+});
+
+Bangle.on('kill', () => {
+  Bangle.setLCDBrightness(1);    
+  Bangle.setLCDTimeout(10);     
+  Bangle.removeAllListeners('lcdPower'); 
+});
