564157999429e108476bb6d0304b71d8daf46cfd
v2lentina <115621639+v2lentina@users.noreply.github.com>
2025-04-17 19:02:03 +0200

    Update vibtimer.app.js

--- apps/vibtimer/vibtimer.app.js
+++ apps/vibtimer/vibtimer.app.js
@@ -1,6 +1,6 @@
 g.clear();
 g.setFont("6x8", 2);
-g.drawString("VibTimerrr", 10, 60);
+g.drawString("VibTimerio", 10, 60);
 Bangle.setLCDTimeout(0);
 Bangle.setPowerSave(false);
 
@@ -8,24 +8,24 @@ const SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
 const CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
 
 NRF.setServices({
-  [SERVICE_UUID]: {
-    [CHARACTERISTIC_UUID]: {
-      writable: true,
-      onWrite: function(evt) {
-        let value = evt.data[0];
-        print("Empfangen:", value);
-        if (value === 1) {
+  [SERVICE]: {
+    [CHAR]: {
+      writable  : true,     
+      maxLen    : 1,
+      description: "Trigger Buzz",
+      onWrite(evt) {
+        if (evt.data[0] === 1) {
           Bangle.buzz(500);
-          g.clear(); g.drawString("Buzz!", 10, 30);
         }
       }
     }
   }
-}, { advertise: [SERVICE_UUID] });
-
-NRF.setAdvertising({}, { name: "Bangle.js VibTimer", connectable: true });
-
-NRF.on('disconnect', () => {
-  print("BLE getrennt – Starte neu...");
-  NRF.setAdvertising({}, { name: "Bangle.js VibTimer", connectable: true });
+}, {
+  advertise : [SERVICE],     
+  uart      : false,        
+  connectable : true
 });
+
+NRF.setTxPower(4);
+
+NRF.on('disconnect', ()=>NRF.restart());
