fa0cfbcad53cd75a6b40fc793bcc1b3081ce2666
v2lentina <115621639+v2lentina@users.noreply.github.com>
2025-04-17 11:06:06 +0200

    Update stable connection

--- apps/vibtimer/vibtimer.app.js
+++ apps/vibtimer/vibtimer.app.js
@@ -8,19 +8,36 @@ Bangle.setPowerSave(false);
 const SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
 const CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
 
+function startAdvertising() {
+  NRF.setAdvertising({}, { name: "Bangle.js VibTimer", connectable: true });
+  print("Werbung aktiv...");
+}
+
 NRF.setServices({
   [SERVICE_UUID]: {
     [CHARACTERISTIC_UUID]: {
       writable: true,
+      notify: true,
       onWrite: function(evt) {
         let data = evt.data;
         let value = data[0];
         print("Empfangen:", value);
 
         if (value === 1) {
+          print("Vibriere jetzt!");
           Bangle.buzz(500);
           g.clear(); g.drawString("Buzz!", 10, 30);
+
+          NRF.updateServices({
+            [SERVICE_UUID]: {
+              [CHARACTERISTIC_UUID]: {
+                value: [66]
+              }
+            }
+          });
+
         } else {
+          print("Kein Buzz");
           g.clear(); g.drawString("Waiting", 10, 30);
         }
       }
@@ -28,16 +45,23 @@ NRF.setServices({
   }
 }, { advertise: [SERVICE_UUID] });
 
-let tick = setInterval(() => {
+let advTicker = setInterval(() => {
+  NRF.getSecurityStatus((s) => {
+    if (!s.connected) startAdvertising();
+  });
+}, 10000);
+
+setInterval(() => {
   g.setFont("6x8");
   g.clearRect(10, 10, 200, 20); 
   g.drawString("Aktiv: " + new Date().toLocaleTimeString(), 10, 10);
-}, 10000);
+}, 15000);
 
 NRF.on('disconnect', () => {
   g.clear();
   g.drawString("BLE getrennt", 10, 30);
   print("BLE getrennt");
+  startAdvertising();
 });
 
 Bangle.on('kill', () => {
