0ad6c15f8b42962357b44d720d012d0b6cad0faf
v2lentina <115621639+v2lentina@users.noreply.github.com>
2025-04-18 10:22:17 +0200

    Update vibtimer.app.js just UI

--- apps/vibtimer/vibtimer.app.js
+++ apps/vibtimer/vibtimer.app.js
@@ -1,31 +1,5 @@
 g.clear();
 g.setFont("6x8", 2);
-g.drawString("VibTimerio", 10, 60);
+g.drawString("VibTimer", 25, 60);
 Bangle.setLCDTimeout(0);
 Bangle.setPowerSave(false);
-
-const SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
-const CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
-
-NRF.setServices({
-  [SERVICE]: {
-    [CHAR]: {
-      writable  : true,     
-      maxLen    : 1,
-      description: "Trigger Buzz",
-      onWrite(evt) {
-        if (evt.data[0] === 1) {
-          Bangle.buzz(500);
-        }
-      }
-    }
-  }
-}, {
-  advertise : [SERVICE],     
-  uart      : false,        
-  connectable : true
-});
-
-NRF.setTxPower(4);
-
-NRF.on('disconnect', ()=>NRF.restart());
